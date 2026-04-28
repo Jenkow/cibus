380c9f11076a47980f8070e3cf61e8fbd59923d6
Jerónimo Auriti Primavera <jeroprimavera@gmail.com>
2026-04-28 16:30:56 -0300

    feat: creation of UserEntity

---
+++ src/main/java/com/jll/cibus/user/UserEntity.java
@@ -0,0 +1,43 @@
+package com.jll.cibus.user;
+
+import com.jll.cibus.branch.BranchEntity;
+import com.jll.cibus.user.UserRoleEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "dni", nullable = false, unique = true)
+    private Long dni;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "phone_number", nullable = false)
+    private Long phoneNumber;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "branch_id")
+    private BranchEntity branch;
+
+    @ManyToOne
+    @JoinColumn(name = "role", nullable = false)
+    private UserRoleEntity role;
+}
