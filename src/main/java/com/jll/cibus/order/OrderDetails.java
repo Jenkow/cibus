1827047e26366fe181f915dc4e4b808347e23cc6
LuSDev <lucianodanielsosa@gmail.com>
2026-04-28 19:49:05 -0300

    Feat: Creation of OrderDetails class

---
+++ src/main/java/com/jll/cibus/order/OrderDetails.java
@@ -0,0 +1,37 @@
+package com.jll.cibus.order;
+
+import com.jll.cibus.product.ProductEntity;
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+import lombok.*;
+
+@Enabled
+@Table(name = "order_details")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class OrderDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "observation",  nullable = true)
+    private String observation;
+
+    @Column(name = "amount") // no es nullable porque por defecto siempre va a tener valor 1 a menos que se indique lo contrario
+    private Integer amount = 1;
+
+    @Column(name = "unit_price", nullable = false)
+    private Double unitPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id",  nullable = false)
+    private OrderEntity order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private ProductEntity product;
+}
