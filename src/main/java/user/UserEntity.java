c563aa9bd2f759baa11e6c66d0008a20b73129ea
Luciano Sosa <lucianodanielsosa@gmail.com>
2026-04-28 12:40:18 -0300

    Feat: Creation of UserEntity class (not finished)

---
+++ src/main/java/user/UserEntity.java
@@ -0,0 +1,20 @@
+package user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // PENDING TO FINISH
+}
