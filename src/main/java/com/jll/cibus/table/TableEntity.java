60b3560119b367b60461edbd24e1951a3f0307a1
Jerónimo Auriti Primavera <jeroprimavera@gmail.com>
2026-04-28 16:03:40 -0300

    Update table entity relationships

--- src/main/java/com/jll/cibus/table/TableEntity.java
+++ src/main/java/com/jll/cibus/table/TableEntity.java
@@ -1,5 +1,6 @@
 package com.jll.cibus.table;
 
+import com.jll.cibus.branch.BranchEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -27,9 +28,9 @@ public class TableEntity {
    //@JoinColumn(name = "waiter_id")
    //private UserEntity waiter;
 
-    //@ManyToOne(fetch = FetchType.LAZY)
-    //@JoinColumn(name = "branch_id", nullable = false)
-    //private BranchEntity branch;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "branch_id", nullable = false)
+    private BranchEntity branch;
 
 
     //CREATE METHODS TO VERIFY THAT EVERY TIME THAT THE TABLE IS NOT AVALIABLE THERE IS A WAITER, AND IF ITS AVALIABLE, THE WAITER IS NULL
