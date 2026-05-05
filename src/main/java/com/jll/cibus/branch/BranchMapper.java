8d80854b2bea1df71d1c8d6f610078c406597bf8
colegioluisinaoc-lab <colegio.luisinaoc@gmail.com>
2026-05-05 12:15:56 -0300

    Creation of Branch DTO: Request & Response

---
+++ src/main/java/com/jll/cibus/branch/BranchMapper.java
@@ -0,0 +1,18 @@
+//package com.jll.cibus.branch;
+//
+//import org.modelmapper.ModelMapper;
+//import org.springframework.stereotype.Component;
+//import org.springframework.ui.Model;
+//
+//@Component
+//public class BranchMapper {
+//
+//    private final ModelMapper modelMapper;
+//
+//    public BranchMapper(ModelMapper modelMapper) {
+//        this.modelMapper = modelMapper;
+//    }
+//
+//
+//
+//}
