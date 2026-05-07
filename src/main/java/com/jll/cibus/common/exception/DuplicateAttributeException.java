7e358333f7df39ac9cc67a186afffc807de48c40
LuSDev <lucianodanielsosa@gmail.com>
2026-05-07 20:02:58 -0300

    Feat: Creation of global exceptions for flexible use (DuplicateAttributeException, UnauthorizedOperationException)

---
+++ src/main/java/com/jll/cibus/common/exception/DuplicateAttributeException.java
@@ -0,0 +1,7 @@
+package com.jll.cibus.common.exception;
+
+public class DuplicateAttributeException extends RuntimeException {
+    public DuplicateAttributeException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/jll/cibus/common/exception/UnauthorizedOperationException.java
@@ -0,0 +1,7 @@
+package com.jll.cibus.common.exception;
+
+public class UnauthorizedOperationException extends RuntimeException {
+    public UnauthorizedOperationException(String message) {
+        super(message);
+    }
+}
