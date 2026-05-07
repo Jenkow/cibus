b1b91efabb233725f972555c7386c679bfe1d9ae
colegioluisinaoc-lab <colegio.luisinaoc@gmail.com>
2026-05-07 13:39:28 -0300

    Fix: change findByName return type to BranchEntity

--- src/main/java/com/jll/cibus/branch/BranchRepository.java
+++ src/main/java/com/jll/cibus/branch/BranchRepository.java
@@ -3,11 +3,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BranchRepository extends JpaRepository<BranchEntity, Long>
 {
-    List<BranchEntity> findByName(String name);
+    Optional<BranchEntity> findByName(String name);
     List<BranchEntity> findByStreetAndNumber (String street, Integer number);
     boolean existsByName(String name);
     boolean existsByStreetAndNumber (String street, Integer number);
