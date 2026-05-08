3ef1ee1e69f2e8e2c7d8869eead9b8a63e12a799
colegioluisinaoc-lab <colegio.luisinaoc@gmail.com>
2026-05-08 00:36:10 -0300

    Creation of User Role Service

---
+++ src/main/java/com/jll/cibus/user/UserRoleService.java
@@ -0,0 +1,32 @@
+package com.jll.cibus.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserRoleService
+{
+    @Autowired
+    private UserRoleRepository userRoleRepository;
+
+    public List<UserRoleEntity> getAllUserRol ()
+    {
+        return userRoleRepository.findAll();
+    }
+
+    private void  idVerification (Long id)
+    {
+        if (!userRoleRepository.existsById(id))
+        {
+            throw new RuntimeException("THERES NO USER ROLE WITH ID "+id);
+        }
+    }
+
+    public void deleteUserRole(Long id)
+    {
+        idVerification(id);
+        userRoleRepository.deleteById(id);
+    }
+}
