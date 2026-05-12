a5b555ac54e6100c509bb8b68c436fd05aa4cff2
Luciano Sosa <lucianodanielsosa@gmail.com>
2026-05-12 13:35:01 -0300

    feat: add product category module

---
+++ src/main/java/com/jll/cibus/product/ProductCategoryEntity.java
@@ -0,0 +1,22 @@
+package com.jll.cibus.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "product_categories")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductCategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+}
