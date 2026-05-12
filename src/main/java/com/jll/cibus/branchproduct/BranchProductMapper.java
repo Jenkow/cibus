08f917636e97f684a7b14537c2a5d46935f5bc37
Jerónimo Auriti Primavera <jeroprimavera@gmail.com>
2026-05-12 12:28:06 -0300

    feat: creation of  BranchProductMapper

--- src/main/java/com/jll/cibus/branchproduct/BranchProductMapper.java
+++ src/main/java/com/jll/cibus/branchproduct/BranchProductMapper.java
@@ -1,4 +1,33 @@
 package com.jll.cibus.branchproduct;
 
-public class BranchProductMapper {
+import com.jll.cibus.common.model.IMapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BranchProductMapper implements IMapper<BranchProductEntity, BranchProductRequestDTO, BranchProductResponseDTO> {
+
+    private final ModelMapper modelMapper;
+
+    public BranchProductMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public BranchProductResponseDTO toDTO (BranchProductEntity entity){
+        BranchProductResponseDTO dto = modelMapper.map(entity, BranchProductResponseDTO.class);
+        dto.setBranchId(entity.getBranch().getId());
+        dto.setBranchName(entity.getBranch().getName());
+        dto.setProductId(entity.getProduct().getId());
+        dto.setProductName(entity.getProduct().getName());
+        return dto;
+    }
+
+    public BranchProductUpdateDTO toUpdateDTO(BranchProductEntity entity){
+        return modelMapper.map(entity, BranchProductUpdateDTO.class);
+    }
+
+    public BranchProductEntity toEntity (BranchProductRequestDTO dto){
+        return modelMapper.map(dto, BranchProductEntity.class);
+    }
+
 }
