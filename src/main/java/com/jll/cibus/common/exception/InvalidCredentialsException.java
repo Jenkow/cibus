170308bec2dfea8908ebcd7916aa7d9d193d857a
Luciano Sosa <lucianodanielsosa@gmail.com>
2026-05-12 11:01:35 -0300

    refactor: Changed the implementation of some exceptions to be more generic.

--- src/main/java/com/jll/cibus/common/exception/InvalidCredentialsException.java
+++ src/main/java/com/jll/cibus/common/exception/InvalidCredentialsException.java
@@ -1,7 +1,7 @@
 package com.jll.cibus.common.exception;
 
 public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) {
+    public InvalidCredentialsException() {
         super("Invalid credentials for operation");
     }
 }
