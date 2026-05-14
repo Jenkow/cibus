a8755e6d88f233a53d806c18940e20c4db119f16
Jerónimo Auriti Primavera <jeroprimavera@gmail.com>
2026-05-14 10:03:43 -0300

    structure fix

--- src/main/java/com/jll/cibus/order/OrderService.java
+++ src/main/java/com/jll/cibus/order/OrderService.java
@@ -1,6 +1,7 @@
 package com.jll.cibus.order;
 
-import com.jll.cibus.user.UserRepository;
+import com.jll.cibus.order.repository.OrderRepository;
+import com.jll.cibus.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
