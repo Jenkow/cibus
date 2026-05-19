1323b96b736638b04fc5cea16f36190ab5cf88e0
colegioluisinaoc-lab <colegio.luisinaoc@gmail.com>
2026-05-19 09:16:04 -0300

    Refactor in ProductCategory Controller

--- src/main/java/com/jll/cibus/productcategory/controller/ProductCategoryController.java
+++ src/main/java/com/jll/cibus/productcategory/controller/ProductCategoryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping ("/product-categories")
+@RequestMapping ("/api/product-categories")
 public class ProductCategoryController {
 
     private final ProductCategoryService productCategoryService;
