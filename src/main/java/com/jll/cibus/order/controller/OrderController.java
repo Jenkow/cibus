e7699840372af93d68f28c7018674ce7ac9b7e5b
colegioluisinaoc-lab <colegio.luisinaoc@gmail.com>
2026-05-19 12:18:09 -0300

    Refactor in Order Controllers end point

--- src/main/java/com/jll/cibus/order/controller/OrderController.java
+++ src/main/java/com/jll/cibus/order/controller/OrderController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/branches")
+@RequestMapping("/api/branches/{branchId}/orders")
 public class OrderController
 {
     private final OrderService orderService;
@@ -22,46 +22,46 @@ public class OrderController
         this.orderService = orderService;
     }
 
-    @GetMapping("{branchId}/orders")
+    @GetMapping()
     public ResponseEntity<List<OrderResponseDTO>> getByBranchId (@PathVariable Long branchId)
     {
         return ResponseEntity.ok(orderService.findByBranchId(branchId));
     }
 
-    @GetMapping ("/{branchId}/orders/table/{tableId}")
+    @GetMapping ("/table/{tableId}")
     public ResponseEntity<List<OrderResponseDTO>> getByTableId (@PathVariable Long tableId)
     {
         return ResponseEntity.ok(orderService.findByTableId(tableId));
     }
-    @GetMapping ("/{branchId}/orders/waiter/{waiterId}")
+    @GetMapping ("/waiter/{waiterId}")
     public  ResponseEntity<List<OrderResponseDTO>> getByWaiterId (@PathVariable Long waiterId)
     {
         return ResponseEntity.ok(orderService.findByWaiterId(waiterId));
     }
-    @GetMapping ("/{branchId}/orders/status/{status}")
+    @GetMapping ("/status/{status}")
     public ResponseEntity<List<OrderResponseDTO>> getByBranchAndStatus (@PathVariable Long branchId, @PathVariable String status)
     {
         return ResponseEntity.ok(orderService.findByBranchAndStatus(branchId, status));
     }
-    @GetMapping ("/{branchId}/orders/paid/{paid}")
+    @GetMapping ("/paid/{paid}")
     public ResponseEntity<List<OrderResponseDTO>> getByPaid (@PathVariable Boolean paid)
     {
         return ResponseEntity.ok(orderService.findByPaid(paid));
     }
-    @PostMapping("/{branchId}/orders")
+    @PostMapping()
     public ResponseEntity<OrderResponseDTO> createOrder (@Valid @RequestBody OrderRequestDTO dto)
     {
         OrderResponseDTO response = orderService.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
-    @PutMapping ("/{branchId}/orders/{orderId}")
+    @PutMapping ("/{orderId}")
     public ResponseEntity<OrderResponseDTO> updateOrder (@PathVariable Long id, @Valid @RequestBody OrderRequestDTO dto)
     {
         return ResponseEntity.ok(orderService.update(dto));
     }
 
     //PROBABLEMENTE SEA MEJOR EL CAMBIAR A ESTADO CANCELADO ASI QUEDA GUARDADO
-    @DeleteMapping ("/{branchId}/orders/{orderId}")
+    @DeleteMapping ("/{orderId}")
     public ResponseEntity<Void> deleteOrder (@PathVariable Long id)
     {
         orderService.delete(id);
