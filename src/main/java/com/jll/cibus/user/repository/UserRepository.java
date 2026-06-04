607dfe189e33331dcc3fd0422af740c5f4bc10a1
Luciano Sosa <lucianodanielsosa@gmail.com>
2026-06-04 11:50:22 -0300

    refactor: added Specification compatibility

--- src/main/java/com/jll/cibus/user/repository/UserRepository.java
+++ src/main/java/com/jll/cibus/user/repository/UserRepository.java
@@ -5,28 +5,19 @@ import com.jll.cibus.user.entity.UserRoleEntity;
 import com.jll.cibus.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity,Long>, JpaSpecificationExecutor<UserEntity>
-{
+public interface UserRepository extends JpaRepository<UserEntity,Long>, JpaSpecificationExecutor<UserEntity> {
 
     List<UserEntity> findByFirstName (String firstName);
 
     List<UserEntity> findByLastName (String lastName);
 
     List<UserEntity> findByFirstNameAndLastName (String firstName, String lastName);
-
-    Optional<UserEntity> findByPhoneNumber (String phoneNumber);
-
-    Optional<UserEntity> findByEmail (String email);
-
-    Optional<UserEntity> findByDni (Long dni);
 
     List<UserEntity> findByBranch (BranchEntity branch);
 
@@ -35,10 +26,16 @@ public interface UserRepository extends JpaRepository<UserEntity,Long>, JpaSpeci
     List<UserEntity> findByRole (UserRoleEntity role);
 
     List<UserEntity> findByRoleId (Long id);
-    
+
+    List<UserEntity> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
+
+    Optional<UserEntity> findByPhoneNumber (String phoneNumber);
+
+    Optional<UserEntity> findByEmail (String email);
+
+    Optional<UserEntity> findByDni (Long dni);
+
     boolean existsByDni (Long dni);
 
     boolean existsByDniAndBranchId(Long dni, Long branchId);
-
-    List<UserEntity> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
 }
