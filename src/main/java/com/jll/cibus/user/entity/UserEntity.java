a70cec75805f412d335697cf06c39667f27d9d4f
colegioluisinaoc-lab <colegio.luisinaoc@gmail.com>
2026-06-10 10:54:35 -0300

    Refactor role management to use JWT roles in User Entity

--- src/main/java/com/jll/cibus/user/entity/UserEntity.java
+++ src/main/java/com/jll/cibus/user/entity/UserEntity.java
@@ -36,7 +36,4 @@ public class UserEntity {
     @JoinColumn(name = "branch_id", nullable = false)
     private BranchEntity branch;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "role_id", nullable = false)
-    private UserRoleEntity role;
 }
