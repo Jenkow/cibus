f5eb021d60d88ed9573bc2e739308fbd869db737
colegioluisinaoc-lab <colegio.luisinaoc@gmail.com>
2026-06-10 11:25:02 -0300

    Fix in role names

--- src/main/java/com/jll/cibus/role/Roles.java
+++ src/main/java/com/jll/cibus/role/Roles.java
@@ -1,9 +1,9 @@
 package com.jll.cibus.role;
 
 public enum Roles {
-    ROLE_ADMIN,
-    ROLE_HOST,
-    ROLE_WAITER,
-    ROLE_KITCHEN,
-    ROLE_MANAGER
+    ADMIN,
+    HOST,
+    WAITER,
+    KITCHEN,
+    MANAGER
 }
