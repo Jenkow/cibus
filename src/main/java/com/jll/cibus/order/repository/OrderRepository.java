405176c4f009d30866a9be81f90c8df5fe2e4e5b
Luciano Sosa <lucianodanielsosa@gmail.com>
2026-06-10 12:51:43 -0300

    feat: added query methods for table metrics

--- src/main/java/com/jll/cibus/order/repository/OrderRepository.java
+++ src/main/java/com/jll/cibus/order/repository/OrderRepository.java
@@ -1,11 +1,14 @@
 package com.jll.cibus.order.repository;
 
 import com.jll.cibus.order.entity.OrderEntity;
-import org.hibernate.query.Order;
+import com.jll.cibus.statistics.dto.table.TableMetricDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -15,5 +18,37 @@ public interface OrderRepository extends JpaRepository<OrderEntity, Long>, JpaSp
     List<OrderEntity> findByBranchIdAndTableId (Long branchId, Long tableId);
     List<OrderEntity> findByBranchIdAndWaiterId (Long branchId, Long waiterId);
     List<OrderEntity> findByBranch_IdAndStatus_Name(Long branchId, String statusName);
+
+    @Query("""
+    SELECT new com.jll.cibus.statistics.dto.table.TableMetricDTO(t.number, COUNT(o))
+    FROM OrderEntity o
+    JOIN o.table t
+    WHERE o.branch.id = :branchId
+    AND o.createdAt BETWEEN :start AND :end
+    GROUP BY t.id, t.number
+    ORDER BY COUNT(o) DESC
+    """)
+    List<TableMetricDTO> findMostBusyTable(Long branchId, LocalDateTime start, LocalDateTime end);
+
+    @Query("""
+    SELECT new com.jll.cibus.statistics.dto.table.TableMetricDTO(t.number, COUNT(o))
+    FROM OrderEntity o
+    JOIN o.table t
+    WHERE o.branch.id = :branchId
+    AND o.createdAt BETWEEN :start AND :end
+    GROUP BY t.id, t.number
+    ORDER BY COUNT(o) ASC
+    """)
+    List<TableMetricDTO> findLessBusyTable(Long branchId, LocalDateTime start, LocalDateTime end);
+
+
+
+    @Query("""
+    SELECT COUNT(DISTINCT o.table.id)
+    FROM OrderEntity o
+    WHERE o.branch.id = :branchId
+    AND o.createdAt BETWEEN :start AND :end
+    """)
+    Long countOccupiedTables(Long branchId, LocalDateTime start, LocalDateTime end);
 
 }
