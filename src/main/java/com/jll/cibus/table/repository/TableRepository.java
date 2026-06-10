53b9ae3340db6a4bc1f70c02d2f00245b6290392
Luciano Sosa <lucianodanielsosa@gmail.com>
2026-06-10 12:57:43 -0300

    feat: added methods

--- src/main/java/com/jll/cibus/table/repository/TableRepository.java
+++ src/main/java/com/jll/cibus/table/repository/TableRepository.java
@@ -4,14 +4,16 @@ import com.jll.cibus.branch.entity.BranchEntity;
 import com.jll.cibus.table.entity.TableEntity;
 import com.jll.cibus.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TableRepository extends JpaRepository<TableEntity, Long> {
-
 
     List<TableEntity> findByBranchId(Long branchId);
 
@@ -19,24 +21,11 @@ public interface TableRepository extends JpaRepository<TableEntity, Long> {
 
     List<TableEntity> findByWaiter_id(Long waiterId);
 
+    Long countByBranchId(Long branchId);
 
     boolean existsById(Long id);
 
     Boolean existsByIdAndBranchId(Long tableId, Long branchId);
 
     boolean existsByBranchIdAndNumber(Long branchId, Integer number);
-
-
-    /* No se usan pero estaban (implementar o borrar):
-
-    List<TableEntity> findByBranch(BranchEntity branch);
-
-    List<TableEntity> findByBranchAndAvailable(BranchEntity branch, Boolean available);
-
-    List<TableEntity> findByBranchIdAndAvailable(Long branchId, Boolean available);
-
-    List<TableEntity> findByBranchAndWaiter(BranchEntity branch, UserEntity waiter);
-
-    List<TableEntity> findByBranchIdAndWaiterId(Long branchId, Long userId);
-     */
 }
