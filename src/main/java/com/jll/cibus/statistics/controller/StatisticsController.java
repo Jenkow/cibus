00bb206a39a5296350e46992d3a5d8b6d1115baf
Luciano Sosa <lucianodanielsosa@gmail.com>
2026-06-10 12:57:30 -0300

    feat: added endpoint methods to StatisticsController

--- src/main/java/com/jll/cibus/statistics/controller/StatisticsController.java
+++ src/main/java/com/jll/cibus/statistics/controller/StatisticsController.java
@@ -1,21 +1,27 @@
 package com.jll.cibus.statistics.controller;
 
+import com.jll.cibus.statistics.dto.table.TableInsightDTO;
 import com.jll.cibus.statistics.service.StatisticsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/statistics")
 public class StatisticsController {
+
     private final StatisticsService statisticsService;
 
-    /*
     @GetMapping("/branch/{branchId}")
-    public ResponseEntity
+    public ResponseEntity<TableInsightDTO> getTableInsights(@PathVariable Long branchId,
+                                                            @RequestParam LocalDateTime start,
+                                                            @RequestParam LocalDateTime end) {
 
-     */
+        return  ResponseEntity.ok(statisticsService.getTableInsights(branchId, start, end));
+    }
 
     // PENDING FEATURES
 }
