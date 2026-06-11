ff9b22063f2c1174e63f0d9e42b6d8bfdf2811f6
Luciano Sosa <lucianodanielsosa@gmail.com>
2026-06-11 16:53:19 -0300

    feat: added endpoints referred to product statistics

--- src/main/java/com/jll/cibus/statistics/controller/StatisticsController.java
+++ src/main/java/com/jll/cibus/statistics/controller/StatisticsController.java
@@ -1,5 +1,6 @@
 package com.jll.cibus.statistics.controller;
 
+import com.jll.cibus.statistics.dto.product.ProductInsightDTO;
 import com.jll.cibus.statistics.dto.table.TableInsightDTO;
 import com.jll.cibus.statistics.service.StatisticsService;
 import lombok.RequiredArgsConstructor;
@@ -15,13 +16,28 @@ public class StatisticsController {
 
     private final StatisticsService statisticsService;
 
-    @GetMapping("/branch/{branchId}")
+    @GetMapping("table/branch/{branchId}")
     public ResponseEntity<TableInsightDTO> getTableInsights(@PathVariable Long branchId,
                                                             @RequestParam LocalDateTime start,
                                                             @RequestParam LocalDateTime end) {
 
-        return  ResponseEntity.ok(statisticsService.getTableInsights(branchId, start, end));
+        return ResponseEntity.ok(statisticsService.getTableInsights(branchId, start, end));
     }
 
-    // PENDING FEATURES
+    @GetMapping("product/branch/{branchId}")
+    public ResponseEntity<ProductInsightDTO> getProductInsights(@PathVariable Long branchId,
+                                                                @RequestParam LocalDateTime start,
+                                                                @RequestParam LocalDateTime end) {
+
+        return ResponseEntity.ok(statisticsService.getProductInsights(branchId, start, end));
+    }
+
+    @GetMapping("product/global")
+    public ResponseEntity<ProductInsightDTO> getGlobalProductInsights(@RequestParam LocalDateTime start,
+                                                                      @RequestParam LocalDateTime end) {
+
+        return ResponseEntity.ok(statisticsService.getGlobalProductInsights(start, end));
+    }
+
+
 }
