1b121b78ff573a4836c43c4703d60a4df1c54a4f
Luciano Sosa <lucianodanielsosa@gmail.com>
2026-06-11 16:53:02 -0300

    feat: added metric methods referred to product statistics

--- src/main/java/com/jll/cibus/statistics/service/StatisticsService.java
+++ src/main/java/com/jll/cibus/statistics/service/StatisticsService.java
@@ -3,6 +3,9 @@ package com.jll.cibus.statistics.service;
 import com.jll.cibus.branchproduct.repository.BranchProductRepository;
 import com.jll.cibus.order.repository.OrderRepository;
 import com.jll.cibus.orderdetail.repository.OrderDetailRepository;
+import com.jll.cibus.statistics.dto.product.ProductCategoryMetricDTO;
+import com.jll.cibus.statistics.dto.product.ProductInsightDTO;
+import com.jll.cibus.statistics.dto.product.ProductMetricDTO;
 import com.jll.cibus.statistics.dto.table.TableInsightDTO;
 import com.jll.cibus.statistics.dto.table.TableMetricDTO;
 import com.jll.cibus.table.repository.TableRepository;
@@ -13,11 +16,9 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class StatisticsService {
-
-    //-------- PARA APROVECHAR AL 100% ESTA FEATURE ES NECESARIO TENER LOGGERS DE TODOS LOS SERVICIOS.
 
     private final OrderRepository orderRepository;
     private final TableRepository tableRepository;
@@ -42,7 +43,7 @@ public class StatisticsService {
                         .findFirst()
                         .orElse(null);
 
-        // Porcentaje de ocupación en el rango de tiempo solicitado.
+        // Calculo de porcentaje de ocupación en el rango de tiempo solicitado.
         Long occupiedTables =
                 orderRepository.countOccupiedTables(branchId, start, end);
 
@@ -58,4 +59,105 @@ public class StatisticsService {
                 .occupancyRate(occupancyRate)
                 .build();
     }
+
+    public ProductInsightDTO getProductInsights(Long branchId, LocalDateTime start, LocalDateTime end) {
+
+        // Producto más vendido.
+        ProductMetricDTO mostPopularProduct = orderDetailRepository.findMostPopularProducts(branchId, start, end)
+                .stream()
+                .findFirst()    // sacamos solo el primero.
+                .orElse(null);
+
+        // Producto menos vendido.
+        ProductMetricDTO lessPopularProduct = orderDetailRepository.findLessPopularProducts(branchId, start, end)
+                .stream()
+                .findFirst()    // sacamos solo el primero.
+                .orElse(null);
+
+        // Producto mas caro.
+        ProductMetricDTO mostExpensiveProduct = orderDetailRepository.findMostExpensiveProducts(branchId, start, end)
+                .stream()
+                .findFirst()    // sacamos solo el primero.
+                .orElse(null);
+
+        // Producto más barato.
+        ProductMetricDTO cheapestProduct = orderDetailRepository.findCheapestProducts(branchId, start, end)
+                .stream()
+                .findFirst()    // sacamos solo el primero.
+                .orElse(null);
+
+        // Categoria mas vendida
+        ProductCategoryMetricDTO mostPopularCategory = orderDetailRepository.findMostPopularCategories(branchId, start, end)
+                .stream()
+                .findFirst()    // sacamos solo el primero.
+                .orElse(null);
+
+        // Categoria menos vendida
+        ProductCategoryMetricDTO lessPopularCategory = orderDetailRepository.findLessPopularCategories(branchId, start, end)
+                .stream()
+                .findFirst()    // sacamos solo el primero.
+                .orElse(null);
+
+        return ProductInsightDTO.builder() // Patron builder para construir el DTO del insight y devolverlo.
+                .mostPopularProduct(mostPopularProduct)
+                .lessPopularProduct(lessPopularProduct)
+                .mostExpensiveProduct(mostExpensiveProduct)
+                .cheapestProduct(cheapestProduct)
+                .mostPopularCategory(mostPopularCategory)
+                .lessPopularCategory(lessPopularCategory)
+                .build();
+    }
+
+    public ProductInsightDTO getGlobalProductInsights(LocalDateTime start, LocalDateTime end) {
+
+        // Producto global más vendido.
+        ProductMetricDTO GlobalMostPopularProduct = orderDetailRepository.findGlobalMostPopularProducts(start, end)
+                .stream()
+                .findFirst()    // sacamos solo el primero.
+                .orElse(null);
+
+        // Producto global menos vendido.
+        ProductMetricDTO GlobalLessPopularProduct = orderDetailRepository.findGlobalLessPopularProducts(start, end)
+                .stream()
+                .findFirst()    // sacamos solo el primero.
+                .orElse(null);
+
+        // Producto global mas caro.
+        ProductMetricDTO GlobalMostExpensiveProduct = orderDetailRepository.findGlobalMostExpensiveProducts(start, end)
+                .stream()
+                .findFirst()    // sacamos solo el primero.
+                .orElse(null);
+
+        // Producto global más barato.
+        ProductMetricDTO GlobalCheapestProduct = orderDetailRepository.findGlobalCheapestProducts(start, end)
+                .stream()
+                .findFirst()    // sacamos solo el primero.
+                .orElse(null);
+
+        // Categoria global mas vendida
+        ProductCategoryMetricDTO GlobalMostPopularCategory = orderDetailRepository.findGlobalMostPopularCategories(start, end)
+                .stream()
+                .findFirst()    // sacamos solo el primero.
+                .orElse(null);
+
+        // Categoria menos vendida
+        ProductCategoryMetricDTO GlobalLessPopularCategory = orderDetailRepository.findGlobalLessPopularCategories(start, end)
+                .stream()
+                .findFirst()    // sacamos solo el primero.
+                .orElse(null);
+
+        return ProductInsightDTO.builder() // Patron builder para construir el DTO del insight y devolverlo.
+                .mostPopularProduct(GlobalMostPopularProduct)
+                .lessPopularProduct(GlobalLessPopularProduct)
+                .mostExpensiveProduct(GlobalMostExpensiveProduct)
+                .cheapestProduct(GlobalCheapestProduct)
+                .mostPopularCategory(GlobalMostPopularCategory)
+                .lessPopularCategory(GlobalLessPopularCategory)
+                .build();
+
+    }
+
+
+
+
 }
