820e3c88f0a0d88a8c2d44748a6fc1a18b08659c
LuSDev <lucianodanielsosa@gmail.com>
2026-06-13 01:55:26 -0300

    feat: added custom query methods to get insights.

--- src/main/java/com/jll/cibus/user/repository/UserRoleRepository.java
+++ src/main/java/com/jll/cibus/user/repository/UserRoleRepository.java
@@ -2,6 +2,8 @@ package com.jll.cibus.user.repository;
 
 import com.jll.cibus.user.entity.UserRoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +13,11 @@ public interface UserRoleRepository extends JpaRepository<UserRoleEntity,Long>
 {
     Optional<UserRoleEntity> findByName (String name);
     boolean existsByName(String name);
+
+    @Query("""
+    SELECT COUNT (u)
+        FROM UserEntity u
+        WHERE u.role.name = :role
+    """)
+    Long countByUserRole (@Param("role") String role);
 }
