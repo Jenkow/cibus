64b285b008c10e339770ffad2fab3f109c73b7ec
LuSDev <lucianodanielsosa@gmail.com>
2026-06-13 01:56:14 -0300

    feat: added metrics DTO for orders and revenues.

--- src/main/java/com/jll/cibus/statistics/dto/order/OrderInsightDTO.java
+++ src/main/java/com/jll/cibus/statistics/dto/order/OrderInsightDTO.java
@@ -1,11 +1,18 @@
 package com.jll.cibus.statistics.dto.order;
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class OrderInsightDTO {
-
+    private BigDecimal totalRevenue;
+    private BigDecimal averageTicket;
+    private BigDecimal averageRevenuePerDay;
+    private OrderMetricDTO highestValueOrder;
+    private OrderMetricDTO lowestValueOrder;
+    private Long totalOrders;
 }
