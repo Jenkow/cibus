5b952ff100974f750dc1d7f385937697c9809ca7
LuSDev <lucianodanielsosa@gmail.com>
2026-06-13 01:56:20 -0300

    feat: added metrics DTO for orders and revenues.

--- src/main/java/com/jll/cibus/statistics/dto/order/OrderMetricDTO.java
+++ src/main/java/com/jll/cibus/statistics/dto/order/OrderMetricDTO.java
@@ -1,11 +1,19 @@
 package com.jll.cibus.statistics.dto.order;
 import lombok.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class OrderMetricDTO {
-
+    private Long orderId;
+    private Long branchId;
+    private Long tableId;
+    private String WaiterName;
+    private BigDecimal ticketValue;
+    private LocalDateTime orderDate;
 }
