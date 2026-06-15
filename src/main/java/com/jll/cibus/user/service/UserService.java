03b852fba99fcdd4ddc85139e9d2ad17cecf5e12
colegioluisinaoc-lab <colegio.luisinaoc@gmail.com>
2026-06-15 14:48:10 -0300

    fix: added import of new exception

--- src/main/java/com/jll/cibus/user/service/UserService.java
+++ src/main/java/com/jll/cibus/user/service/UserService.java
@@ -23,6 +23,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.jll.cibus.common.exception.ForbiddenOperationException;
 
 import java.util.Arrays;
 import java.util.HashSet;
